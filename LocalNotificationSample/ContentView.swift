9c91d4644aa1d57ed261a78bfdd8d2d13e79e2a8
Taka <takapohny@gmail.com>
2024-11-08 10:51:29 -0800

    1st commit

--- LocalNotificationSample/ContentView.swift
+++ LocalNotificationSample/ContentView.swift
@@ -8,14 +8,113 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    enum NotificationPermissionStatus {
+        case notDetermined, authorized, denied
+    }
+    
+    @State private var permissionSatatus: NotificationPermissionStatus = .notDetermined
+    @Environment(\.scenePhase) var scenePhase
+    var notificationManager: NotificationManager
+    
+    init() {
+        let manager = NotificationManager()
+        self.notificationManager = manager
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack(spacing:30) {
+                switch permissionSatatus {
+                    case .notDetermined:
+                        Button("Request Permission") {
+                            Task {
+                                let result = await notificationManager.askNotificationPermission()
+                                permissionSatatus = result ? .authorized : .denied
+                            }
+                        }
+                        .buttonStyle(.bordered)
+                    case .denied:
+                        Button("Open Settings") {
+                            Task {
+                                await openSettings()
+                            }
+                        }
+                        .buttonStyle(.bordered)
+                    case .authorized:
+                        Button("Schedule Notification") {
+                            Task {
+                                await notificationManager.scheduleNotification()
+                            }
+                        }
+                        .buttonStyle(.borderedProminent)
+                }
+                List{
+                    ForEach(notificationManager.pendingRequests, id: \.identifier) { request in
+                        HStack {
+                            Text(request.content.title)
+                            Spacer()
+                            Text(request.identifier)
+                                .font(.caption)
+                                .foregroundStyle(.secondary)
+                        }
+                        .swipeActions {
+                            Button("Delete", systemImage: "minus.circle", role: .destructive) {
+                                Task {
+                                    await notificationManager.removeRequest(identifier: request.identifier)
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+                
+            }
+            .padding(.top, 200)
+            .onChange(of: scenePhase) { _, phase in
+                if phase == .active {
+                    Task {
+                        let status = await notificationManager.getNotifucationStatus()
+                        switch status {
+                        case .notDetermined:
+                            permissionSatatus = .notDetermined
+                        case .authorized:
+                            permissionSatatus = .authorized
+                        case .denied:
+                            permissionSatatus = .denied
+                        default:
+                            break
+                        }
+                        
+                        await notificationManager.getPengingRequests()   // update request pendding list
+                    }
+                }
+            }
+            .navigationTitle("Notification")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button{
+                        Task {
+                            await notificationManager.clearAllRequests()
+                        }
+                        
+                    } label: {
+                        Image(systemName: "clear.fill")
+                            .imageScale(.large)
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    
+    /// open privacy setting to set notification on
+    func openSettings() async{
+        guard
+            let settingsURL = URL(string: UIApplication.openSettingsURLString),
+            UIApplication.shared.canOpenURL(settingsURL)
+        else { return }
+        
+        await UIApplication.shared.open(settingsURL)
     }
 }
 
---
+++ LocalNotificationSample/NotificationManager.swift
@@ -0,0 +1,86 @@
+//
+//  NotificationManager.swift
+//  LocalNotificationSample
+//
+//  Created by Taka on 2024-11-08.
+//
+
+import Foundation
+import UserNotifications
+
+@MainActor
+@Observable
+class NotificationManager: NSObject, UNUserNotificationCenterDelegate {
+    let notificationCenter = UNUserNotificationCenter.current()
+    var pendingRequests: [UNNotificationRequest] = [] // notificaitons on stack
+    
+    override init() {
+        super.init()
+        notificationCenter.delegate = self
+    }
+    
+    /// for delegate. required to show notification even while the app is active.
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+        await getPengingRequests() // update request pendding list
+        return [.sound, .banner]
+    }
+    
+    /// notification authorization request
+    func askNotificationPermission() async -> Bool{
+        let task = Task {
+            return try await notificationCenter.requestAuthorization(options: [.alert, .badge, .sound])
+        }
+        let result = await task.result
+        switch result {
+        case .success(let allowed):
+            return allowed
+        case .failure (let error):
+            print("Permission denied: \(error.localizedDescription)")
+            return false
+        }
+    }
+    
+    /// obtain authorization status (notDetermind, authorized, denied)
+    func getNotifucationStatus() async -> UNAuthorizationStatus{
+        let setting = await notificationCenter.notificationSettings()
+        return setting.authorizationStatus
+    }
+    
+    /// add scheduled notification
+    func scheduleNotification() async{
+        let content = UNMutableNotificationContent()
+        content.title = "Feed the cat"
+        content.body = "It looks hangry"
+        content.sound = .default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        let task = Task {
+            try await notificationCenter.add(request)
+        }
+        let result = await task.result
+        if case .success = result {
+            print("Notification scheduled")
+            await getPengingRequests() // update request pendding list
+        }
+    }
+    
+    /// update pending notificaion list
+    func getPengingRequests() async {
+        pendingRequests = await notificationCenter.pendingNotificationRequests()
+        print("Pending requests: \(pendingRequests.count)")
+    }
+    
+    /// cancel specified notification
+    func removeRequest(identifier: String) async {
+        notificationCenter.removePendingNotificationRequests(withIdentifiers: [identifier])
+        await getPengingRequests()  // update request pendding list
+    }
+    
+    /// cancel all notification
+    func clearAllRequests() async {
+        notificationCenter.removeAllPendingNotificationRequests()
+        await getPengingRequests()  // update request pendding list
+    }
+}
